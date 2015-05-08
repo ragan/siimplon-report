c38af444c9f8395683191682129fb4f92ded9096
Karol Peek <peek.karol@gmail.com>
2015-05-08 14:16:44 +0200

    plotman plotman

--- siimplon-app/app/models/Project.java
+++ siimplon-app/app/models/Project.java
@@ -1,24 +1,27 @@
 package models;
 
-import play.data.validation.MinSize;
 import play.data.validation.Required;
+import play.db.jpa.Blob;
 import play.db.jpa.Model;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.List;
 
 @Entity
 public class Project extends Model {
 
     @Required
-    @MinSize(4)
     public String name;
 
-    public Project(String name) {
-        this.name = name;
-    }
+    public String description;
 
-    @Override
-    public String toString() {
-        return String.format("Project: %s", name);
+    @OneToMany(cascade = CascadeType.ALL)
+    public List<ProjectMap> projectMaps;
+
+    public Project(String name, String description) {
+        this.name = name;
+        this.description = description;
     }
 }
